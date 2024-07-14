a5a51f13eeae574bb82d24779ec72f946e4e4c17
Dejan <dejan.ristovski.1@students.finki.ukim.mk>
2024-07-14 23:15:27 +0200

    Update parsed products

--- chatbot/data_preperation/product_parser.py
+++ chatbot/data_preperation/product_parser.py
@@ -9,25 +9,26 @@ from langchain_openai import ChatOpenAI
 
 from output_parsers import description_parser, Description
 
-YOUR_PRODUCTS_FOLDER_PATH = "C:/Users/Dejan/Documents/GitHub/Chatbot/chatbot/products/"
-YOUR_PARSED_PRODUCTS_FOLDER_PATH = "C:/Users/Dejan/Documents/GitHub/Chatbot/chatbot/parsed_products/"
+YOUR_PRODUCTS_FOLDER_PATH = "C:/Users/drist/Documents/GitHub/Chatbot/chatbot/products/"
+YOUR_PARSED_PRODUCTS_FOLDER_PATH = "C:/Users/drist/Documents/GitHub/Chatbot/chatbot/parsed_products/"
 
 if __name__ == '__main__':
     # load environment variables
     load_dotenv()
 
+    # indexes of products that caught an error
+    error_indexes = []
+
     # iterate each json file
     for index, fname in enumerate(glob(YOUR_PRODUCTS_FOLDER_PATH + "*.json")):
-
-        # parse only the first 10 products
-        if index >= 10:
-            break
-
         # read file
         with open(fname, 'r', encoding='utf-8') as f:
 
             # load to json
             product = json.load(f)
+
+            # get file name
+            file_name = fname.split("\\")[1]
 
             # create template
             summary_template = """
@@ -50,18 +51,28 @@ if __name__ == '__main__':
             # create chain
             chain = summary_prompt_template | llm | description_parser
 
-            # invoke chain
-            result: Description = chain.invoke(input={"description": product["description"]})
+
+            has_error = False
+            # invoke chain, if an error is caught print to document and continue
+            try:
+                result: Description = chain.invoke(input={"description": product["description"]})
+            except:
+                print(f"Error in product {index}.")
+                error_indexes.append(index)
+                f = open("errors.txt", "a")
+                f.write(f"Product {index} error: {file_name}\n")
+                has_error = True
+                continue
 
             # add parsed description to json
             product["description"] = result.description
 
-            # get file name
-            file_name = fname.split("\\")[1]
-
             # save to directory
-            path = os.getcwd()
             with open(YOUR_PARSED_PRODUCTS_FOLDER_PATH + file_name, 'w', encoding='utf-8') as f:
                 json.dump(product, f, ensure_ascii=False, indent=4)
 
-            print(f"Product {index} parsed.")
+            if not has_error:
+                print(f"Product {index} parsed.")
+    # after for loop, print all error indexes list to document for debugging
+    f = open("errors.txt", "a")
+    f.write(f"Errors in products:\n{str(error_indexes)}\n")
